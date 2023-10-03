6f84051b5fa36ffa2b85854a72c6937fd9a9a2a5
artwoojin <artwoojin@gmail.com>
2023-10-03 20:54:36 +0900

    chapter 7-51~52 anonymous class

---
+++ src/main/java/org/JavaJungseokEssence/Chapter7/Ex7_17.java
@@ -0,0 +1,10 @@
+package org.JavaJungseokEssence.Chapter7;
+
+public class Ex7_17 {
+    Object iv = new Object() { void method(){} };       // 익명 클래스
+    static Object cv = new Object(){ void method(){}};  // 익명 클래스
+
+    void myMethod() {
+        Object lv = new Object() { void method(){}};    // 익명 클래스
+    }
+}
