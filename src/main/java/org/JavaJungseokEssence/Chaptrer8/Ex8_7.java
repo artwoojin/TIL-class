29f25f66ab85d8958e4d2e225f2d68a323cf3abc
artwoojin <artwoojin@gmail.com>
2023-10-05 21:29:00 +0900

    chapter 8-08~14 Exception -2

---
+++ src/main/java/org/JavaJungseokEssence/Chaptrer8/Ex8_7.java
@@ -0,0 +1,8 @@
+package org.JavaJungseokEssence.Chaptrer8;
+
+public class Ex8_7 {
+    public static void main(String args[]) {
+        // throw new Exception();
+        throw new RuntimeException();
+    }
+}
