f0b917ef9455f044fdf88d3a85d8ba3573e68945
artwoojin <artwoojin@gmail.com>
2024-03-04 10:26:42 +0900

    folder change

---
+++ JavaJungSeoks/src/main/java/org/JavaJungseokEssence/Chapter8/Ex8_7.java
@@ -0,0 +1,8 @@
+package org.JavaJungseokEssence.Chapter8;
+
+public class Ex8_7 {
+    public static void main(String args[]) {
+        // throw new Exception();
+        throw new RuntimeException();
+    }
+}
---
+++ JavaJungSeoks/src/main/java/org/JavaJungseokEssence/Chapter8/Ex8_8.java
@@ -0,0 +1,7 @@
+package org.JavaJungseokEssence.Chapter8;
+
+public class Ex8_8 {
+    public static void main(String args[]) {
+        throw new RuntimeException();
+    }
+}
