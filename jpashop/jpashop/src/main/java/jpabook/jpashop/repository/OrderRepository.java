18673817a03a144e88f8ebaa1d4f4a1a485424ae
artwoojin <artwoojin@gmail.com>
2024-10-12 12:49:40 +0900

    S7. 주문 서비스 개발

--- jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -15,7 +15,7 @@ public class OrderRepository {
         em.persist(order);
     }
 
-    public Order findOner(Long id) {
+    public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
 
